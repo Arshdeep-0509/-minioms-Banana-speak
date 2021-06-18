13bec03d9ddd0239be03302794d7c72a20f64e51
Arshdeep singh <71091109+Arshdeep-0509@users.noreply.github.com>
2021-06-18 23:42:04 +0530

    import javascript

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("hello script this is arshdeep singh");
+
+// alert
+
+// alert("press ok to play the game ")
+
+
+ var username = prompt("Give me  your username ");
+ alert("The script works " + username);
+
+ 

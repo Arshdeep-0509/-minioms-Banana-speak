064577ef17a80d2bdff78eaeba3c4b2ebb12317b
Arshdeep singh <71091109+Arshdeep-0509@users.noreply.github.com>
2021-06-20 01:10:12 +0530

    taking input and console

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
-console.log("hello script this is arshdeep singh");
+// console.log("hello script this is arshdeep singh");
 
 // alert
 
 // alert("press ok to play the game ")
 
 
- var username = prompt("Give me  your username ");
- alert("The script works " + username);
+//  var username = prompt("Give me  your username ");
+//  alert("The script works " + username);
 
- 
+
+
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+
+console.log(txtInput);
+
+function clickHandler (){
+
+    console.log("clicked");
+    console.log("input" , txtInput.value);
+
+};
+
+btnTranslate.addEventListener("click", clickHandler);
+
+// document.querySelector   : it is an API
+// addEventListener    :  it is also an API
+
+// API: application programming interface. This interface will be using applications

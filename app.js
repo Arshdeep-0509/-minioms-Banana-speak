968ba5292aa70faabafd2412e879b3b7e24d26a1
arshdeep <arshdeepsinghkhalsa2001@gmail.com>
2021-08-08 13:08:42 +0530

    banana-speak

--- app.js
+++ app.js
@@ -1,33 +1,23 @@
-// console.log("hello script this is arshdeep singh");
-
-// alert
-
-// alert("press ok to play the game ")
-
-
-//  var username = prompt("Give me  your username ");
-//  alert("The script works " + username);
-
-
-
+  
 var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
-
-
-   
-
-
-
-function clickHandler (){
- outputDiv.innerText= "hbfehifvnreifjh eruif " + txtInput.value;
-
-
-};
-
-btnTranslate.addEventListener("click", clickHandler);    //this object is called document object model
-
-// document.querySelector   : it is an API
-// addEventListener    :  it is also an API
-
-// API: application programming interface. This interface will be using applications
+var  txtInput= document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output-txt");
+var serverURL= "https://api.funtranslations.com/translate/minion.json";
+function getTranslatedURL(text){
+    return serverURL + "?" + "text=" + text
+}
+function errorHandler(error){
+    console.log("error",error)
+    alert("Ooops! something went wrong")
+}
+function clickHandler(){
+var inputText= txtInput.value;    
+fetch(getTranslatedURL(inputText))
+.then(response => response.json())
+.then(json =>{
+    var translatedText=json.contents.translated;
+    outputDiv.innerText= translatedText;
+})
+.catch(errorHandler)
+}
+btnTranslate.addEventListener("click",clickHandler);
